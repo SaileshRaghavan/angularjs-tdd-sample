2630396e39761668866a30b5c0226fdcca96d952
Leena <leena.sn@multunus.com>
2014-02-10 19:33:34 +0530

    Add sinatra support

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require "sinatra/json"
+
+set :public_folder, File.dirname(__FILE__) + '/build'
+
+get '/' do
+	File.read(File.join('build', 'index.html'))    
+end
+
+get '/:handle/followers' do
+	json :data => "
+    {
+        \"followers\": [
+            {
+                \"id\": 0,
+                \"name\": \"twitter\"
+            },
+            {
+                \"id\": 1,
+                \"name\": \"github\"
+            }
+        ]
+    }"
+end
